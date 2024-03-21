0a5439f847d87b645832d7f83cb98287ac02292a
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2024-03-21 21:44:07 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,1 +1,6 @@
-
+python3 manage.py runserver
+python3 manage.py makemigrations
+python3 manage.py createsuperuser
+python3 manage.py migrate
+python3 manage.py startapp appname
+python manage.py changepassword <username>
