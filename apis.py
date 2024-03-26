91845f8af78739efd1f8bd2f8b76ca7823a53eae
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2024-03-26 15:34:08 +0300

    Update apis.py

--- apis.py
+++ apis.py
@@ -2,4 +2,10 @@
 # this to return with no exception 
 about = About.objects.filter(id=id).first()  # Retrieve the object or None if not found
 about = About.objects.get(id=id)  #this returns exception when the object not found
+about = About.objects.all  #to return all objects
+about.delete() # to delete an object
+about.save # to create or update
+
+
+
 
