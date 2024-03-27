d05b4cf5967ea29ba060e359bb9b3f80ef55ecb0
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2024-03-27 14:54:44 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,7 @@
-
+sudo service postgresql status
+sudo service postgresql start
+sudo systemctl stop postgresql
 sudo -u postgres psql
 CREATE USER <username> WITH PASSWORD '<password>';
 CREATE DATABASE django_todo OWNER <username>;
+deactivate
