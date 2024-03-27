238dc93d0257ef3db6cd71a3eafec11b15eb4301
aderawmolla <103243862+aderawmolla@users.noreply.github.com>
2024-03-27 15:20:30 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,3 +3,10 @@ docker images
 docker ps
 docker run -d -p 8000:8000 my-django-app
 docker --version
+
+<h1>Good Blog to Learn Both Django Docker and </h1>
+
+
+docker compose build
+docker stop $(docker ps -a -q)
+docker compose up
